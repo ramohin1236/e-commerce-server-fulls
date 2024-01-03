3b5daf0864131b5c74d69d5398bc635d2aae2224
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-01-02 23:09:31 -0800

    pagination

--- controller/productController.js
+++ controller/productController.js
@@ -89,7 +89,7 @@ const getallProduct = asyncHandler(async(req,res)=>{
             query = query.sort("-createdAt")
         }
 
-        // limiting the fields
+        // limiting the fields-----------------
           if(req.query.fields){
             const fields = req.query.fields.split(",").join(" ");
             query = query.select(fields)
@@ -99,6 +99,16 @@ const getallProduct = asyncHandler(async(req,res)=>{
             query = query.select("-__v")
           }
 
+        // pagination-------------------------------
+         const page = req.query.page;
+         const limit = req.query.limit;
+         const skip =(page -1) * limit;
+         query = query.skip(skip).limit(limit);
+         if(req.query.page){
+            const productCount = await Product.countDocuments();
+            if(skip >=productCount) throw new Error("This page is no exists")
+         }
+         console.log(page,limit,skip);
 
         const findProduct = await query
         res.json(findProduct)
