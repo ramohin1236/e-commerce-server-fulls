3cabfb19195b6b1cf5306178b7b2119aa546399d
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-01-04 12:05:45 -0800

    product wishlist added

--- controller/productController.js
+++ controller/productController.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler');
 const Product = require("../models/productModels")
 const slugify = require('slugify');
 const validateMongoDbId = require('../utils/validateMongodb');
+const User = require('../models/userModels');
 
 
 const createProduct = asyncHandler(async(req,res)=>{
@@ -118,11 +119,39 @@ const getallProduct = asyncHandler(async(req,res)=>{
      }
 })
 
-
+ const addToWishList = asyncHandler(async(req,res)=>{
+    const {_id} = req.user;
+    const {prodId} =req.body;
+    try{
+        const user = await User.findById(_id);
+        const alreadyAdded = user.wishlist.find((id)=>id.toString()=== prodId);
+        if(alreadyAdded){
+           let user = await User.findByIdAndUpdate(_id,{
+            $pull: {wishlist: prodId}
+           },{
+            new: true
+           })
+           res.json(user)
+        }
+        else{
+            let user = await User.findByIdAndUpdate(_id,{
+                $push: {wishlist: prodId}
+               },{
+                new: true
+               })
+               res.json(user)
+        }
+        
+    }
+    catch(error){
+        throw new Error(error)
+    }
+ })
 module.exports={
     createProduct,
     getaProduct,
     getallProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    addToWishList
 }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createProduct,getaProduct,getallProduct,updateProduct,deleteProduct } = require('../controller/productController');
+const { createProduct,getaProduct,getallProduct,updateProduct,deleteProduct,addToWishList } = require('../controller/productController');
 const { isAdmin, authMiddleWare } = require('../middlewears/authMiddelware');
 
 const router = express.Router()
@@ -7,6 +7,8 @@ const router = express.Router()
 router.post('/',authMiddleWare,isAdmin, createProduct)
 router.get('/', getallProduct)
 router.get('/:id', getaProduct)
+router.put('/wishlist',authMiddleWare, addToWishList)
+
 router.put("/:id",authMiddleWare,isAdmin,updateProduct);
 router.delete("/:id",authMiddleWare,isAdmin,deleteProduct);
 
