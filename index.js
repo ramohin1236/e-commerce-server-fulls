b6ab4f7798e79d4fa68d4fe148fab887c814ebe4
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-02-12 10:17:46 -0800

    product brand

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const bodyParser = require('body-parser');
 const { whenNotFound, errorHandlerr, } = require('./middlewears/erroHandler.js');
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
-
+const cors = require('cors')
 
 const app = express();
 const dotenv = require("dotenv").config()
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 dbConnection() 
 app.use(morgan("dev"))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(cookieParser())
 
